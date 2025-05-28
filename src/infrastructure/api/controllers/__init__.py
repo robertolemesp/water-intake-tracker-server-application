fd9828aea4040bdf1b851588af78814d73cc188d
Roberto <robertolemesp@hotmail.com>
2025-05-28 10:37:13 -0300

    Adds controller aggregation

---
+++ src/infrastructure/api/controllers/__init__.py
@@ -0,0 +1,9 @@
+from infrastructure.api.controllers.user import UserController
+from infrastructure.api.controllers.water_intake import WaterIntakeController
+from infrastructure.api.controllers.user_hydration_summary import UserHydrationSummaryController
+
+__all__ = [
+  'UserController',
+  'WaterIntakeController',
+  'UserHydrationSummaryController'
+]
